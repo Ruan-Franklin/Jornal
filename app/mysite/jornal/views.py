c49ca979d189526e812b582e40abf5781e3dc2e9
Ruan-Franklin <ruanfranklinmengo@gmail.com>
2023-02-04 22:26:58 -0300

    Adicionando viewsets

--- app/mysite/jornal/views.py
+++ app/mysite/jornal/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Autor, Artigo
+from .serializers import AutorSerializer, ArtigoSerializer
+'''
+Este arquivo inclui dois ViewSets, um para cada modelo. O AutorViewSet lida com todas as operações para o modelo Autor e o ArtigoViewSet lida com todas as operações para o modelo Artigo. O atributo queryset define o queryset padrão para o ViewSet, que são todas as instâncias do modelo associado. O atributo serializer_class define o serializador a ser usado com o ViewSet. Observe que os serializadores não são definidos neste arquivo, eles precisam ser importados de um arquivo serializers.py separado.
+'''
 
-# Create your views here.
+class AutorViewSet(viewsets.ModelViewSet):
+    '''Classe para lidar com operações para o modelo de Autor'''
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class ArtigoViewSet(viewsets.ModelViewSet):
+    queryset = Artigo.objects.all()
+    serializer_class = ArtigoSerializer
+
+
