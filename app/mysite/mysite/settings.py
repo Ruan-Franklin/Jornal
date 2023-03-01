f7996c0447ad99e6281cc8fdc77462b271e35936
Ruan Franklin <ruanfranklinmengo@gmail.com>
2023-03-01 16:28:46 +0000

    Reformando front

--- app/mysite/mysite/settings.py
+++ app/mysite/mysite/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [    '/app/frontend/dist',    '/app/frontend/src',    'app/frontend/src/components']
+STATICFILES_DIRS = [    '/workspace/Jornal/app/frontend/dist',    '/workspace/Jornal/app/frontend/src',    '/workspace/Jornal/app/frontend/src/components',]
 
 
 
