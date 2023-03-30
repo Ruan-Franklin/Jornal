6ec88bdbc5ffc215402a7788a7ad61ab248bf5f0
Ruan Franklin <ruanfranklinmengo@gmail.com>
2023-03-30 00:29:40 +0000

    Adicionar filtros na página admin (#7)
    Este commit adiciona filtros à página admin para que os usuários possam pesquisar e filtrar os dados exibidos. Com isso, a issue #7 é fechada automaticamente.

--- app/mysite/jornal/admin.py
+++ app/mysite/jornal/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-
 from .models import Artigo, Autor
 
-admin.site.register(Autor)
-admin.site.register(Artigo)
+class AutorAdmin(admin.ModelAdmin):
+    list_filter = ['nome', 'email']
+
+class ArtigoAdmin(admin.ModelAdmin):
+    list_filter = ['autor__nome', 'data_pub']
+
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Artigo, ArtigoAdmin)
--- app/mysite/mysite/settings.py
+++ app/mysite/mysite/settings.py
@@ -147,3 +147,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#CSRF TOKEN
+CSRF_TRUSTED_ORIGINS = ['https://8000-ruanfranklin-jornal-ltpeils3gbd.ws-us93.gitpod.io']
